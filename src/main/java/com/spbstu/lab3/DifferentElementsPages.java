86fdab0aa320bebe63a80c8378e0b68a9f432435
kvakersha <polina81297@gmail.com>
2018-06-02 17:19:30 +0300

    Add files via upload

---
+++ src/main/java/com/spbstu/lab3/DifferentElementsPages.java
@@ -0,0 +1,123 @@
+package com.spbstu.lab3;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.actions;
+import org.openqa.selenium.support.FindBy;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class DifferentElementsPages {
+
+    @FindBy(css = ".dropdown-menu | [href=\"page8.htm\"]")
+    SelenideElement DifferentElementsPageButton;
+
+    @FindBy(css = ".label-checkbox")
+    ElementsCollection CheckBoxes;
+
+    @FindBy(css = ".label-radio")
+    ElementsCollection Radios;
+
+    @FindBy(css = "div.colors")
+    ElementsCollection DropDownColors;
+
+    @FindBy(css = "[value=\"Default Button\"]")
+    SelenideElement DefaultButton;
+
+    @FindBy(css = "[value=\"Button\"]")
+    SelenideElement Button;
+
+    @FindBy(css = ".logs li")
+    ElementsCollection Logs;
+
+    @FindBy(css = "#mCSB_1")
+    SelenideElement LeftSection;
+
+    @FindBy(css = "#mCSB_2")
+    SelenideElement RightSection;
+
+    @FindBy(css = ".dropdown-menu | [href=\"page4.htm\"]")
+    SelenideElement DatesPageButton;
+
+    @FindBy(css = ".ui-slider-handle")
+    ElementsCollection Sliders;
+
+
+    public DifferentElementsPages(){Selenide.page(this);}
+
+
+    public void areDiffBoxexExists() {
+        for(int i=0; i<4; i++){
+            CheckBoxes.get(i).shouldBe(Condition.visible);
+            Radios.get(i).shouldBe(Condition.visible);
+        }
+        DropDownColors.get(0).shouldBe(Condition.visible);
+        LeftSection.shouldBe(Condition.visible);
+        RightSection.shouldBe(Condition.visible);
+        DefaultButton.shouldBe(Condition.visible);
+        Button.shouldBe(Condition.visible);
+    }
+
+    public void selectBoxElements(String str) {
+        for (int i = 0; i < 4; i++) {
+            if (CheckBoxes.get(i).getText().contains(str)) {
+                CheckBoxes.get(i).click();
+            }
+            if (Radios.get(i).getText().contains(str)) {
+                Radios.get(i).click();
+            }
+        }
+    }
+
+    public void selectColorElement(String str){
+        $(".colors .uui-form-element").selectOption(str);
+    }
+
+    public void checkSelectedElements(String[] strAr){
+        for(int i=0; i<6; i++){
+            Logs.get(6-i-1).shouldHave(Condition.text(strAr[i]));
+        }
+    }
+
+    public void checkUnselectedElements(String[] strAr){
+        for (int i = 0; i < Logs.size();i++)
+            System.out.println(Logs.get(i).getText());
+
+        for(int i=4; i<6; i++){
+            Logs.get(i).shouldHave(Condition.text(strAr[6-5-i]));
+        }
+    }
+
+    public void slideToPositions(int left, int right) {
+        Integer length = 274;
+        Integer leftBegin = Integer.parseInt(Sliders.get(0).getText());
+        Integer rightBegin = Integer.parseInt(Sliders.get(1).getText());
+
+        if (left <= leftBegin) {
+            actions().dragAndDropBy(Sliders.get(0), -((length * ((leftBegin - left) + 1) / 100)), 0).perform();
+            System.out.println("Left: " + Integer.parseInt(Sliders.get(0).getText()));
+        } else {
+            // actions().dragAndDropBy(Sliders.get(0), (length * ((-leftBegin + left) - 1) / 100), 0).perform();
+            // System.out.println("Left: "+Integer.parseInt(Sliders.get(0).getText()));
+            while (leftBegin != left) {
+                actions().dragAndDropBy(Sliders.get(0), (leftBegin + 1), 0).perform();
+                leftBegin = Integer.parseInt(Sliders.get(0).getText());
+            }
+            if (right < rightBegin) {
+                actions().dragAndDropBy(Sliders.get(1), -(length * ((rightBegin - right) + 1) / 100), 0).perform();
+                System.out.println("Right: " + Integer.parseInt(Sliders.get(1).getText()));
+            } else {
+                actions().dragAndDropBy(Sliders.get(1), (length * ((-rightBegin + right) + 1) / 100), 0).perform();
+                System.out.println("Right: " + Integer.parseInt(Sliders.get(1).getText()));
+            }
+            System.out.println("");
+            System.out.println("Left: " + Integer.parseInt(Sliders.get(0).getText()));
+            System.out.println("Right: " + Integer.parseInt(Sliders.get(1).getText()));
+
+            Sliders.get(0).shouldHave(Condition.text(String.valueOf(left)));
+            Sliders.get(1).shouldHave(Condition.text(String.valueOf(right)));
+        }
+    }
+}
--- src/main/java/com/spbstu/lab3/HomePageSelenide.java
+++ src/main/java/com/spbstu/lab3/HomePageSelenide.java
@@ -1,11 +1,11 @@
 package com.spbstu.lab3;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
-import java.util.List;
-
+import com.spbstu.lab3.utilities.TestConfig;
 
 public class HomePageSelenide {
 
@@ -25,10 +25,10 @@ public class HomePageSelenide {
     SelenideElement LogOutIn;
 
     @FindBy(css = ".icons-benefit")
-    List<SelenideElement> IconsIn;
+    ElementsCollection IconsIn;
 
     @FindBy(css = ".benefit-txt")
-    List<SelenideElement> TextsIn;
+    ElementsCollection TextsIn;
 
     @FindBy(css = ".main-title")
     SelenideElement MainTitleIn;
@@ -36,16 +36,22 @@ public class HomePageSelenide {
     @FindBy(css = ".main-txt")
     SelenideElement MainTextIn;
 
+    @FindBy(css = "li.sub-menu")
+    SelenideElement ServiceLeftSection;
+
+    @FindBy(css = "li.dropdown")
+    SelenideElement ServiceHeader;
+
     public HomePageSelenide(){
         Selenide.page(this);
     }
 
-    public static void open() {
-        Selenide.open("https://jdi-framework.github.io/tests/index.htm");
+    public static void open(TestConfig cfg) {
+        Selenide.open(cfg.homepage());
     }
 
-    public void getTitle() {
-        Selenide.title();
+    public String getTitle() {
+        return Selenide.title();
     }
 
     public void isLoggedIn(String Login, String Password) {
@@ -56,24 +62,34 @@ public class HomePageSelenide {
         LogOutIn.shouldBe(Condition.visible);
     }
 
-    public String getUsername() {
-        return ProfileIcon.getText();
+    public void isUserNameOK(String Username){
+        ProfileIcon.shouldBe(Condition.visible);
+        ProfileIcon.shouldHave(Condition.text(Username));
     }
 
-    public int getIconsNumber() {
-        //System.out.println(IconsIn.size());
-        return IconsIn.size();
+    public void areIconsAndTextsExists() {
+        for(int i=0; i<4; i++) {
+            IconsIn.get(i).shouldBe(Condition.visible);
+            TextsIn.get(i).shouldBe(Condition.visible);
+        }
     }
 
-    public String getPictureText(int i) {
-        return TextsIn.get(i).getText();
+    public void isMainTitleOK(String mainTitle) {
+        MainTitleIn.shouldBe(Condition.visible);
+        MainTitleIn.shouldHave(Condition.text(mainTitle));
     }
 
-    public String getMainTitle() {
-        return MainTitleIn.getText();
+    public void isMainTextOK(String mainText) {
+        MainTextIn.shouldBe(Condition.visible);
+        MainTextIn.shouldHave(Condition.text(mainText));
     }
 
-    public String getMainText() {
-        return MainTextIn.getText();
+    public void areSreviceButtonsOK(String[] str){
+        ServiceLeftSection.click();
+        ServiceHeader.click();
+        for(int i=0; i<6; i++){
+            ServiceLeftSection.shouldHave(Condition.text(str[i]));
+            ServiceHeader.shouldHave(Condition.text(str[i]));
+        }
     }
 }
